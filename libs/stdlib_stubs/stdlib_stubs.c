cd7153eb73d017a8943885f3624e0ea09f962cdc
Till Billerbeck <tillbillerbeck@protonmail.com>
2025-04-26 07:41:54 +0200

    add new cmsis layer to suppress warnings about missing stdlib functions

---
+++ libs/stdlib_stubs/stdlib_stubs.c
@@ -0,0 +1,12 @@
+
+#include "stdlib_stubs.h"
+
+#define __RETURN_NOTHING { return ; }
+
+void _read_r(void) __RETURN_NOTHING
+void _lseek_r(void) __RETURN_NOTHING
+void _kill_r(void) __RETURN_NOTHING
+void _isatty_r(void) __RETURN_NOTHING
+void _getpid_r(void) __RETURN_NOTHING
+void _fstat_r(void) __RETURN_NOTHING
+void _close_r(void) __RETURN_NOTHING
---
+++ libs/stdlib_stubs/stdlib_stubs.h
@@ -0,0 +1,13 @@
+#ifndef STDLIB_STUBS_H
+#define STDLIB_STUBS_H
+
+void _read_r(void);
+void _lseek_r(void);
+void _kill_r(void);
+void _isatty_r(void);
+void _getpid_r(void);
+void _fstat_r(void);
+void _close_r(void);
+
+#endif // STDLIB_STUBS_H
+
