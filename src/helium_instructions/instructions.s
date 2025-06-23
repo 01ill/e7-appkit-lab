862755a9b10a4041caad1074ec2338d2573d72a7
Till Billerbeck <tillbillerbeck@protonmail.com>
2025-06-23 14:01:08 +0200

    add microbenchmark for branching instructions

--- src/helium_instructions/instructions.s
+++ src/helium_instructions/instructions.s
@@ -4,6 +4,8 @@
     .size label, (. - label)
 .endm
 
+.p2align 3
+
 .global generateFpStall
 .type generateFpStall, %function
 
@@ -29,4 +31,82 @@ testPredication:
     vldrwt.f32 q1, [r0, #16]
     vmult.f32 q2, q0, q1
     vstrwt.f32 q2, [r0]
+    pop {pc}
+
+.global branch1
+.type branch1, %function
+
+branch1:
+    push {lr}
+    mov r1, #0
+
+loopStartB1:
+    vfma.f32 q0, q1, q2
+    vfma.f32 q1, q2, q3
+    add r1, #1
+    vfma.f32 q2, q3, q4
+    cmp r1, r0
+    vfma.f32 q3, q4, q5
+
+    blt loopStartB1
+
+    pop {pc}
+
+.global branch2
+.type branch2, %function
+
+branch2:
+    push {lr}
+    mov r1, #0
+
+loopStartB2:
+    cmp r1, r0
+    bge b2End
+
+    vfma.f32 q0, q1, q2
+    vfma.f32 q1, q2, q3
+    add r1, #1
+    vfma.f32 q2, q3, q4
+    vfma.f32 q3, q4, q5
+
+    b loopStartB2
+b2End:
+    pop {pc}
+
+
+.global branchLOB
+.type branchLOB, %function
+
+branchLOB:
+    push {lr}
+    
+    dls lr, r0
+
+loopStartLOB:
+    vfma.f32 q0, q1, q2
+    vfma.f32 q1, q2, q3
+    vfma.f32 q2, q3, q4
+    vfma.f32 q3, q4, q5
+
+    le lr, loopStartLOB
+
+    pop {pc}
+
+.global branchCBZ
+.type branchCBZ, %function
+
+branchCBZ:
+    push {lr}
+
+cbzStart:
+    cbz r0, cbzEnd
+
+    vfma.f32 q0, q1, q2
+    vfma.f32 q1, q2, q3
+    subs r0, #1
+    vfma.f32 q2, q3, q4
+    vfma.f32 q3, q4, q5
+    b cbzStart
+
+cbzEnd:
     pop {pc}
--- src/helium_instructions/main.cpp
+++ src/helium_instructions/main.cpp
@@ -1,22 +1,78 @@
+#include <cstdint>
 #include <ctime>
 #include "RTE_Components.h"
+#include "benchmark.hpp"
 #include "profiling.hpp"
 #include CMSIS_device_header
+#include "SEGGER_RTT.h"
+#include <cstdio>
+#include "timing.hpp"
+#include "LPRTC.hpp"
+#include "fault_handler.h"
+
+
+static char PRINTF_OUT_STRING[256] __attribute__((used, section(".bss.array_region_sram0")));
 
 extern "C" {
     void generateFpStall(float * input);
     void testPredication(float * input);
+    void branch1(uint32_t count);
+    void branch2(uint32_t count);
+    void branchLOB(uint32_t count);
+    void branchCBZ(uint32_t count);
 }
 
 int main (void) {
+   	fault_dump_enable(true);
+	SEGGER_RTT_ConfigUpBuffer(0, nullptr, nullptr, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+	LPRTC::getInstance().enable();
+
     float hallo[8] = {0, 1, 2, 3, 4, 5, 6, 7};
-    setupProfilingMVEStalls();
-    startCounting();
-    generateFpStall(hallo);
-    stopCounting();
-    printCounter();
+    // setupProfilingMVEStalls();
+    // startCounting();
+    // generateFpStall(hallo);
+    // stopCounting();
+    // printCounter();
+    uint32_t time;
+    uint32_t iterations = 100000000;
+    RTC_Clock::time_point start;
+    RTC_Clock::time_point end;
 
     testPredication(hallo);
+    SEGGER_RTT_printf(0, "Test;TimeAbs;Count\n");
+
+    start = RTC_Clock::now();
+    branch1(iterations);
+    end = RTC_Clock::now();
+    time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    sprintf(PRINTF_OUT_STRING, "BranchBackward;%d;%d\r\n", time, iterations);
+    SEGGER_RTT_WriteString(0, PRINTF_OUT_STRING);
+    
+    start = RTC_Clock::now();
+    branch2(iterations);
+    end = RTC_Clock::now();
+    time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    sprintf(PRINTF_OUT_STRING, "BranchForward;%d;%d\r\n", time, iterations);
+    SEGGER_RTT_WriteString(0, PRINTF_OUT_STRING);
+
+    start = RTC_Clock::now();
+    branchLOB(iterations);
+    end = RTC_Clock::now();
+    time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    sprintf(PRINTF_OUT_STRING, "BranchLOB;%d;%d\r\n", time, iterations);
+    SEGGER_RTT_WriteString(0, PRINTF_OUT_STRING);
+
+    start = RTC_Clock::now();
+    branchCBZ(iterations);
+    end = RTC_Clock::now();
+    time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+    sprintf(PRINTF_OUT_STRING, "BranchCBZ;%d;%d\r\n", time, iterations);
+    SEGGER_RTT_WriteString(0, PRINTF_OUT_STRING);
+
 
     while (1) __WFI();
 }
